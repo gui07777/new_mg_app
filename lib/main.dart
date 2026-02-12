8cadcab70681e04a94d00b5f29eab31286dc5e3a
gui07777 <guilherme2006.gmc@gmail.com>
2026-02-12 09:29:28 -0300

    start app

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:new_mg_app/app/app_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Bar();
+  }
+
+  
+}
+
